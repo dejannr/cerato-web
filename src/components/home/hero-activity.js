6fe00c393eaaa06701e6e6065fbf503d53ce6f67
dejannr <radovanovic.dejan2804@gmail.com>
2026-09-08 01:17:08 +0200

    commit;

--- src/components/home/hero-activity.js
+++ src/components/home/hero-activity.js
@@ -9,10 +9,9 @@ export function HeroActivity() {
 
   return <div className={`activity-strip${isOpen ? " is-open" : ""}`}>
     <button className="activity-toggle" type="button" aria-expanded={isOpen} aria-controls={activityId} onClick={() => setIsOpen((open) => !open)}>
-      <span>Activity</span>
-      <ChevronDown aria-hidden="true" />
+      <span className="activity-toggle-line"><span>Activity</span><ChevronDown aria-hidden="true" /></span>
+      <span className="activity-hint">Click to view activity</span>
     </button>
-    <p className="activity-hint">Click to view activity</p>
     <div className="activity-events" id={activityId} hidden={!isOpen}>
       <p><time>08:27</time> Request received</p>
       <p><time>08:29</time> Driver assigned</p>
